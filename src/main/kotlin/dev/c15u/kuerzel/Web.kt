7de897f7f9aa61675f2ab0e79eee009dd667d5e8
Christophe '116' Loiseau <116@lab0.net>
2024-02-07 20:34:53 +0100

    Highlight result

--- src/main/kotlin/dev/c15u/kuerzel/Web.kt
+++ src/main/kotlin/dev/c15u/kuerzel/Web.kt
@@ -47,7 +47,41 @@ class Web(val service: Service) {
     }.toString()
   }
 
-  inline fun HtmlBlockTag.resultsTable(results: Either<String, List<Abbreviation>>) {
+  fun extract(string: String, highlight: String): List<String> {
+    if(highlight.isEmpty()) {
+      return listOf(string)
+    }
+
+    var s = string
+    val res = mutableListOf<String>()
+
+    while (s.isNotEmpty()) {
+      val i = s.indexOf(highlight, ignoreCase = true)
+      when (i) {
+        -1 -> {
+          res.add(s)
+          s = ""
+        }
+
+        0 -> {
+          res.add(s.take(highlight.length))
+          s = s.drop(highlight.length)
+        }
+
+        else -> {
+          res.add(s.substring(0, i))
+          s = s.drop(i)
+        }
+      }
+    }
+
+    return res
+  }
+
+  private inline fun HtmlBlockTag.resultsTable(
+    results: Either<String, List<Abbreviation>>,
+    highlight: String? = null
+  ) {
     div(classes = "container") {
       div(classes = "row justify-content-center") {
         id = "search-results"
@@ -61,8 +95,8 @@ class Web(val service: Service) {
             {
               it.forEach { a ->
                 tr {
-                  td { +a.abbreviation }
-                  td { +a.full }
+                  highlightedTableDivision(highlight, a.abbreviation)
+                  highlightedTableDivision(highlight, a.full)
                 }
               }
             }
@@ -72,10 +106,28 @@ class Web(val service: Service) {
     }
   }
 
+  private fun TR.highlightedTableDivision(highlight: String?, cellContent:String) {
+    td {
+      if (highlight != null) {
+        for (s in extract(cellContent, highlight)) {
+          if (s.equals(highlight, ignoreCase = true)) {
+            span(classes = "highlight") {
+              +s
+            }
+          } else {
+            +s
+          }
+        }
+      } else {
+        +cellContent
+      }
+    }
+  }
+
   fun filter(query: String): String {
     return buildString {
       appendHTML().div {
-        resultsTable(service.search(query))
+        resultsTable(service.search(query), query)
       }
     }
   }
